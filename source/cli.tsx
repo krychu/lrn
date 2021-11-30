34321a514b1ae0f6a393e195797434759c5ff658
krychu <samp.krystian@gmail.com>
2021-11-30 09:10:41 +0100

    lala

--- source/cli.tsx
+++ source/cli.tsx
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
 
-//import fs from "fs";
+import fs from "fs";
+//import React from 'react';
 import React from 'react';
+// import {Box} from 'ink';
 import {render} from 'ink';
 //import meow from 'meow';
 import App from './ui';
-import { RecoilRoot } from "recoil";
 
 // const cli = meow(`
 // 	Usage
@@ -25,57 +26,93 @@ import { RecoilRoot } from "recoil";
 // 	}
 // });
 
-// export interface Card {
-// 	question: string;
-// 	answer: string;
+export interface Card {
+	question: string;
+	answer: string;
+	goodCnt: number;
+};
+
+function readCards(filename: string): Card[] {
+	const text = fs.readFileSync(filename, {encoding: "utf8"});
+	const lines = text.split(/\n/);
+	const cards: Card[] = [];
+
+	let lastState: "separator" | "question" | "answer" = "separator";
+	let question: string = "";
+	let answer: string = "";
+	for (let i=0; i<lines.length; i++) {
+		const line = (lines[i] as string).trim();
+		const emptyLine = line.length === 0;
+		const lastLine = i === lines.length-1;
+		if (lastState === "separator") {
+			if (!emptyLine) {
+				lastState = "question";
+				question = line;
+			} // else lastState correctly remains "separator"
+		} else if (lastState === "question") {
+			if (emptyLine) {
+				throw new Error(`Answer should follow the question (line: ${i})`);
+			} else {
+				lastState = "answer";
+				answer = line;
+				const card: Card = {question, answer, goodCnt: 0};
+				cards.push(card);
+			}
+		} else if (lastState === "answer") {
+			if (emptyLine) {
+				lastState = "separator";
+			} else {
+				throw new Error(`Empty line should follow question (line: ${i})`);
+			}
+		}
+
+		if (lastLine && lastState === "question") {
+			throw Error(`Last question doesn't have an answer`);
+		}
+	}
+
+	return cards;
+}
+
+const cards = readCards("cards/german.txt");
+
+// const FullScreen: FC = (props) => {
+// 	const [size, setSize] = useState({
+// 		columns: process.stdout.columns,
+// 		rows: process.stdout.rows,
+// 	});
+
+// 	useEffect(() => {
+// 		function onResize() {
+// 			setSize({
+// 				columns: process.stdout.columns,
+// 				rows: process.stdout.rows,
+// 			});
+// 		}
+
+// 		process.stdout.on("resize", onResize);
+// 		process.stdout.write("\x1b[?1049h");
+// 		return () => {
+// 			process.stdout.off("resize", onResize);
+// 			process.stdout.write("\x1b[?1049l");
+// 		};
+// 	}, []);
+
+// 	return (
+// 		<Box width={size.columns} height={size.rows}>
+// 			{props.children}
+// 		</Box>
+// 	);
 // };
 
-// function readCards(filename: string) {
-// 	const text = fs.readFileSync(filename, {encoding: "utf8"});
-// 	const lines = text.split(/\n/);
-// 	const cards: Card[] = [];
+const enterAltScreenCommand = "\x1b[?1049h";
+const leaveAltScreenCommand = "\x1b[?1049l";
+process.stdout.write(enterAltScreenCommand);
+process.on("exit", () => {
+  process.stdout.write(leaveAltScreenCommand);
+});
 
-// 	let lastState: "separator" | "question" | "answer" = "separator";
-// 	let question: string = "";
-// 	let answer: string = "";
-// 	for (let i=0; i<lines.length; i++) {
-// 		const line = (lines[i] as string).trim();
-// 		const emptyLine = line.length === 0;
-// 		const lastLine = i === lines.length-1;
-// 		if (lastState === "separator") {
-// 			if (!emptyLine) {
-// 				lastState = "question";
-// 				question = line;
-// 			} // else lastState correctly remains "separator"
-// 		} else if (lastState === "question") {
-// 			if (emptyLine) {
-// 				throw new Error(`Answer should follow the question (line: ${i})`);
-// 			} else {
-// 				lastState = "answer";
-// 				answer = line;
-// 				const card: Card = {question, answer};
-// 				cards.push(card);
-// 			}
-// 		} else if (lastState === "answer") {
-// 			if (emptyLine) {
-// 				lastState = "separator";
-// 			} else {
-// 				throw new Error(`Empty line should follow question (line: ${i})`);
-// 			}
-// 		}
-
-// 		if (lastLine && lastState === "question") {
-// 			throw Error(`Last question doesn't have an answer`);
-// 		}
-// 	}
-
-// 	return cards;
-// }
-
-//const cards = readCards("cards/german.txt");
 //render(<App name={cli.flags.name}/>);
 render(
-    <RecoilRoot>
-        <App />
-    </RecoilRoot>
+		<App cards={cards} goodCnt={2} />
 );
--- source/ui.tsx
+++ source/ui.tsx
@@ -1,9 +1,7 @@
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react';
 import {Text, Box, useInput, useApp} from 'ink';
-//import { Card } from "./cli";
-import { useRecoilState } from "recoil";
+import { Card } from "./cli";
 import chalk from "chalk";
-import { cardsState, userInputState, Card } from "./state";
 
 /*
    {
@@ -22,49 +20,110 @@ import { cardsState, userInputState, Card } from "./state";
    delete: false,
    meta: false
    }
-*/
+ */
 
-   /* const App: FC<{cards2: Card[]}> = ({cards2 = []}) => { */
-const App: FC<{}> = () => {
-	  const {exit} = useApp();
-	  /* const [cards, setCards] = useState(_cards); */
+type Step = "question" | "good-answer" | "bad-answer" | "end";
 
-	  //const [card] = useState(cards[0] as Card);
-	  /* const [input, setInput] = useState(""); */
-	  //const [text, setText] = useState("");
-    const [userInput, setUserInput] = useRecoilState(userInputState);
-    const [cards] = useRecoilState(cardsState);
+/* const App: FC<{cards2: Card[]}> = ({cards2 = []}) => { */
+const App: FC<{cards: Card[], goodCnt: number}> = ({cards = [], goodCnt = 1}) => {
+	const {exit} = useApp();
 
-	  /* const [progress, setProgress] = useState({cardCnt: cards.length, answeredCnt: 0}); */
+	/* const [cards, setCards] = useState(_cards); */
 
-	  useInput((input, key) => {
-		    if (input === 'q') {
-			      exit();
-		    }
+	//const [card] = useState(cards[0] as Card);
+	/* const [input, setInput] = useState(""); */
+	//const [text, setText] = useState("");
+  const [userInput, setUserInput] = useState("");
+	//const [card] = useState(cards[0] as Card);
+	const [allCards] = useState(cards);
+	//const [cardIdx, setCardIdx] = useState(0);
+	const [step, setStep] = useState("question" as Step)
+	const [card, setCard] = useState(cards[0] as Card);
 
-		    if (key.leftArrow) {
-		    } else if (key.return) {
-            setUserInput("");
-            return;
-		    } else if (key.delete) {
-            setUserInput(userInput.slice(0, userInput.length-1));
-            return;
-        }
+	/* const [progress, setProgress] = useState({cardCnt: cards.length, answeredCnt: 0}); */
 
-		    setUserInput(userInput + input);
-	  });
+	const getNextCard = () => {
+		const candidateCards = allCards.filter(card => card.goodCnt < goodCnt);
+		if (!candidateCards.length) {
+			return null;
+		}
+		const idx = Math.floor(Math.random() * candidateCards.length);
+		return candidateCards[idx];
+	};
 
-	  return <Box justifyContent="center">
-		    <Box flexBasis="50%" flexDirection="column" borderStyle="single">
-			      <Box paddingX={1}>
-				        <Text>{(cards[0] as Card).question}</Text>
-			      </Box>
-			      <Box paddingX={1} paddingTop={1}>
-				        <Text>{userInput}{chalk.inverse(" ")}</Text>
-			      </Box>
-		    </Box>
-	  </Box>
+	const questionInput = (input: string, key: any) => {
+		if (key.return) {
+			if (userInput.toLowerCase() === card.answer.toLowerCase()) {
+				//const newCards = [...cards];
+				card.goodCnt++;
+				//setCards(newCards);
+				setStep("good-answer");
+			} else {
+				setStep("bad-answer");
+			}
+			return;
+		} else if (key.delete) {
+			setUserInput(userInput.slice(0, userInput.length-1));
+			return;
+		}
+		setUserInput(userInput + input);
+	};
+
+	const answerInput = (_: string, key: any) => {
+		if (key.return) {
+			const nextCard = getNextCard();
+			setUserInput("");
+			if (nextCard) {
+				setCard(nextCard);
+				setStep("question");
+			} else {
+				setStep("end");
+			}
+		}
+	};
+
+	useInput((input, key) => {
+		if (input === 'q') {
+			exit();
+		}
+
+		switch (step) {
+			case "question":
+				questionInput(input, key);
+				break;
+		  case "bad-answer":
+			case "good-answer":
+				answerInput(input, key);
+				break;
+			default:
+				throw new Error("?");
+		}
+	});
+
+	const badAnswer = step === "bad-answer" ? <Box paddingX={1}>
+		<Text color="red">{card.answer}</Text>
+	</Box> : null;
+
+	const userInputDisplay = userInput + (step === "question" ? chalk.inverse(" ") : "");
+
+	return <Box justifyContent="center">
+		<Box flexBasis="50%" flexDirection="column">
+			<Box flexDirection="column" borderStyle="single">
+				<Box paddingX={1}>
+					<Text>{card.question}</Text>
+				</Box>
+				<Box paddingX={1} paddingTop={1} height={2}>
+					<Text>{userInputDisplay}</Text>
+				</Box>
+			</Box>
+			{badAnswer}
+		</Box>
+	</Box>
 };
+
+/* function isCorrect(question: string, answer: string): boolean {
+ * 	return question.toLowerCase() === answer.toLowerCase();
+ * } */
 
 module.exports = App;
 export default App;
