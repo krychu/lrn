0f6becfeb3bba0ed8e9b1da74182124d4247e601
Krystian <ksamp@esri.com>
2021-12-04 13:43:41 +0100

    lala

--- source/design.ts
+++ source/design.ts
@@ -1,5 +1,54 @@
 
+type Step = Question | Answer;
 
+const [step, setStep] = useState(null);
+
+function onReturn() {
+    if (isQuestionStep()) {
+        
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+useInput((input, key) => {
+    if (key.return) {
+        nextStep();
+    } else if (isQuestionStep()) {
+        step.answer += input;
+    }
+});
+
+function gotoNextStep() {
+    const steps = [gotoQuestionStep, gotoAnswerStep, gotoEndStep];
+    for (const step of steps) {
+        if (step()) {
+            return;
+        }
+    }
+}
+
+function gotoQuestionStep() {
+}
+
+// problem, how do we pass answer? through step
+function gotoAnswerStep() {
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+// How to start, without input?
+useInput((input, key) => {
+    if (key.return && isAnswerStep()) {
+        if (hasMoreCards()) {
+            gotoQuestionStep();
+        } else {
+            gotoEndStep();
+        }
+    } else if (key.return && isQuestionStep()) {
+        gotoAnswerStep(answer);
+    } else {}
+});
 
 HERE
 useInput((input, key) => {
@@ -7,3 +56,4 @@ useInput((input, key) => {
         gotoNext();
     }
 });
+
